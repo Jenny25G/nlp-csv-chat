7c7b3771a58f4b04393b6c202888819320266278
Jenny25G <jeni25us@gmail.com>
2024-03-14 18:06:39 +0530

    nlp-project

---
+++ langchain-ask-csv-main/main.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from langchain_experimental.agents import create_csv_agent
+from langchain_openai import OpenAI
+
+
+
+def main():
+
+    load_dotenv()
+        # Load the OpenAI API key from the environment variable
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
+    else:
+        print("OPENAI_API_KEY is set")
+
+
+    st.set_page_config(page_title="Z-Query")
+    st.header("Z-Query")
+
+    user_csv = st.file_uploader("Upload a CSV file", type="csv")
+
+    if user_csv is not None:
+        user_question = st.text_input("Ask a question about your CSV: ")
+
+        llm = OpenAI(temperature=0)
+        agent = create_csv_agent(llm, user_csv, verbose=True)
+
+        if user_question is not None and user_question != "":
+            response = agent.run(user_question)
+            st.write(response)
+
+
+if __name__ == "__main__":
+    main()
